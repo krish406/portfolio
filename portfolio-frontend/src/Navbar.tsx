f10ec14340ddc831f9aa3a1535824a3c6e4ed24b
krish406 <155498135+krish406@users.noreply.github.com>
2025-09-01 12:36:48 -0400

    added glassy styling

--- portfolio-frontend/src/Navbar.tsx
+++ portfolio-frontend/src/Navbar.tsx
@@ -1,41 +1,37 @@
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="text-white text-xl">Krish Shah</div>
-      <ul className="flex space-x-6">
-        <li>
-          <a
-            href="#about"
-            className="link"
-          >
-            About
-          </a>
-        </li>
-        <li>
-          <a
-            href="#experience"
-            className="link"
-          >
-            Experience
-          </a>
-        </li>
-        <li>
-          <a
-            href="#projects"
-            className="link"
-          >
-            Projects
-          </a>
-        </li>
-        <li>
-          <a
-            href="#contact"
-            className="link"
-          >
-            Contact
-          </a>
-        </li>
-      </ul>
+      <div className="mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <span className="text-white text-xl">Krish Shah</span>
+          <div className="hidden md:flex items-center space-x-10">
+            <a
+              href="#about"
+              className="link"
+            >
+              About
+            </a>
+            <a
+              href="#experience"
+              className="link"
+            >
+              Experience
+            </a>
+            <a
+              href="#projects"
+              className="link"
+            >
+              Projects
+            </a>
+            <a
+              href="#contact"
+              className="link"
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
