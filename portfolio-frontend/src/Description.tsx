1810444a0c562199bed6c7cbe27b715c8cf2025c
krish406 <155498135+krish406@users.noreply.github.com>
2025-09-11 21:06:44 -0400

    fixed image import

--- portfolio-frontend/src/Description.tsx
+++ portfolio-frontend/src/Description.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { RevealOnScroll } from "./RevealOnScroll";
 
